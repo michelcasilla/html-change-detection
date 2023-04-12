0fdb879f88beeabab8e9e4661925456b97536bd8
Michel Casilla <efficientmedia@gmail.com>
2023-04-12 16:08:14 -0400

    tmp log

--- dist/validate.js
+++ dist/validate.js
@@ -1,10 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.validateEnv = void 0;
+const logger_1 = require("./logger");
 const validateEnv = (requiredEnvVars) => {
     for (const envVar of requiredEnvVars) {
         if (!process.env[envVar]) {
             throw new Error(`Missing environment variable: ${envVar}`);
+        }
+        else {
+            (0, logger_1.logWithColor)(`${envVar}: ${process.env[envVar]}`, 'green');
         }
     }
 };
--- src/validate.ts
+++ src/validate.ts
@@ -1,7 +1,11 @@
+import { logWithColor } from "./logger";
+
 export const validateEnv = (requiredEnvVars: string[])=>{
 	for (const envVar of requiredEnvVars) {
 		if (!process.env[envVar]) {
-		throw new Error(`Missing environment variable: ${envVar}`);
+			throw new Error(`Missing environment variable: ${envVar}`);
+		}else{
+			logWithColor(`${envVar}: ${process.env[envVar]}`, 'green');
 		}
 	}
 }
