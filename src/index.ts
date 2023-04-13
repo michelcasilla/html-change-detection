796c360afae999c5845f718b7c189836c0ee093d
Michel Casilla <efficientmedia@gmail.com>
2023-04-13 10:32:17 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import * as path from 'path';
 import { HtmlChangeDetector } from './htmlChangeDetector';
-import { logWithColor } from './logger';
 import { validateEnv } from './validate';
 
 (async () => {
@@ -17,10 +16,6 @@ import { validateEnv } from './validate';
   const slackChannel = process.env.HTML_SLACK_CHANNEL;
   const committedBy = process.env.COMMITTER;
   const branchUrl = process.env.BRANCH_URL;
-
-  // const atPath = process.env.AUTOMATE_TESTING_REPO_PATH;
-  // logWithColor(`PR_BASE_REF: ${base}`, 'green');
-  // logWithColor(`SLACK_BOT_TOKEN: ${token}`, 'green');
   const detector = new HtmlChangeDetector(token, slackChannel, base, committedBy, branchUrl);
   const diffFilePath = path.join(__dirname, 'diff.txt');
   await detector.processDiffFile(diffFilePath);
